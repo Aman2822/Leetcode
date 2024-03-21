318de87f437153cc31c19b6048837f3f7720a680
Aman2822 <132164788+Aman2822@users.noreply.github.com>
2024-03-21 14:37:29 +0530

    Time: 6 ms (48.86%) | Memory: 45.4 MB (91.59%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int start = 0;
+        int end = matrix[0].length-1; 
+
+        while(start<matrix.length && end>=0){
+            if(matrix[start][end] == target){
+                return true;
+            }
+            else if(target < matrix[start][end]){
+                end--;
+            }else{
+                start++;
+            }
+        }
+        return false;
+    }
+}
