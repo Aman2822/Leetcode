02d22c02c1a32501e612bc3231902adee558d51a
Aman2822 <132164788+Aman2822@users.noreply.github.com>
2025-04-20 02:30:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (25.09%) - LeetSync

---
+++ 500-keyboard-row/keyboard-row.cpp
@@ -0,0 +1,32 @@
+
+class Solution {
+public:
+    vector<string> findWords(vector<string>& words) {
+        // Create a mapping of each letter to its corresponding row (1, 2, or 3)
+        unordered_map<char, int> letterToRow = {
+            {'q', 1}, {'w', 1}, {'e', 1}, {'r', 1}, {'t', 1}, {'y', 1}, {'u', 1}, {'i', 1}, {'o', 1}, {'p', 1},
+            {'a', 2}, {'s', 2}, {'d', 2}, {'f', 2}, {'g', 2}, {'h', 2}, {'j', 2}, {'k', 2}, {'l', 2},
+            {'z', 3}, {'x', 3}, {'c', 3}, {'v', 3}, {'b', 3}, {'n', 3}, {'m', 3}
+        };
+        
+        vector<string> result;
+        
+        for (const string& word : words) {
+            int row = letterToRow[tolower(word[0])];  // Get the row of the first letter
+            bool canBeTyped = true;
+            
+            for (char c : word) {
+                if (letterToRow[tolower(c)] != row) {  // If the current character doesn't match the row
+                    canBeTyped = false;
+                    break;
+                }
+            }
+            
+            if (canBeTyped) {
+                result.push_back(word);
+            }
+        }
+        
+        return result;
+    }
+};
