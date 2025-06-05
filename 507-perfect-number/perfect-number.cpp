60f1e98b3d3e1bc3f8c462fa3252152f3913471f
Aman2822 <132164788+Aman2822@users.noreply.github.com>
2025-06-05 13:43:03 +0530

    Time: 2085 ms (20.77%) | Memory: 7.8 MB (22.68%) - LeetSync

--- 507-perfect-number/perfect-number.cpp
+++ 507-perfect-number/perfect-number.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        if (num <= 1) return false; // A perfect number is a positive integer greater than 1.
-        
-        int sum = 1; // 1 is always a divisor of any number, so we start with 1.
-        
-        // Iterate over numbers from 2 to sqrt(num)
-        for (int i = 2; i * i <= num; ++i) {
-            if (num % i == 0) {  // i is a divisor of num
+        int sum = 0;
+        for(int i=1;i< num;i++){
+            if(num%i==0){
                 sum += i;
-                if (i != num / i) {  // Avoid adding the square root twice if it's perfect square
-                    sum += num / i;
-                }
             }
-        }
-        
-        return sum == num; // Check if sum of divisors equals num
-    }
+        } 
+        if(num==sum) return true;
+        else return false;
+    }
 };
