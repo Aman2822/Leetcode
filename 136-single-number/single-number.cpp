0a0f475247fd631dd7f0b2372405ccb30af817db
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-06-14 01:27:25 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (82.91%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -3,19 +3,29 @@ public:
     int singleNumber(vector<int>& nums) {
         int n = nums.size();
         
-        unordered_map<int,int> hashh;
+        // unordered_map<int,int> hashh;
 
-        for(int i = 0 ; i< n ;i++){
-            hashh[nums[i]]++;
+        // for(int i = 0 ; i< n ;i++){
+        //     hashh[nums[i]]++;
+        // }
+
+        // for(int i = 0; i<n ;i++){
+        //     if(hashh[nums[i]] == 1){
+        //         return nums[i];
+        //     }
+        // }
+
+        // return -1;
+
+
+        //Optimal 
+
+        int ans  = 0;
+
+        for(int i = 0 ; i < n; i++){
+            ans = ans ^ nums[i];
         }
-
-        for(int i = 0; i<n ;i++){
-            if(hashh[nums[i]] == 1){
-                return nums[i];
-            }
-        }
-
-        return -1;
         
+        return ans;
     }
 };
