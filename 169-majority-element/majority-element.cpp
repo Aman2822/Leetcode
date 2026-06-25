3988854ddbbbf77dae4685d08733fa1be6430626
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-06-25 19:42:52 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (64.18%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -2,20 +2,47 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int, int> mpp;
-        // int max_Apperance  = (n/2) + 1;
-        // int count  = 0;
+    //     unordered_map<int, int> mpp;
+    //     // int max_Apperance  = (n/2) + 1;
+    //     // int count  = 0;
 
-        for(int i = 0 ;  i < n ; i++  ){
-           mpp[nums[i]]++;
+    //     for(int i = 0 ;  i < n ; i++  ){
+    //        mpp[nums[i]]++;
+    //     }
+
+    //     for(auto it : mpp){
+    //         if(it.second > n/2){
+    //             return it.first;
+    //         }
+    //     }
+    //    return -1;
+
+
+    //Moore's voting algo
+    int count = 0;
+    int el;
+
+    for(int i= 0 ; i < n ; i++){
+        if(count == 0){
+            count++;
+            el = nums[i];
         }
-
-        for(auto it : mpp){
-            if(it.second > n/2){
-                return it.first;
+        else if(nums[i] == el) {
+            count++;
             }
+        else {
+            count--;
         }
-       return -1;
+    }
+    int count1 = 0;
+    for(int i = 0 ; i < n ; i++){
+        if(nums[i] == el) count1++;
+
+        if(count1 > n/2){
+            return el;
+        }
+    }
+    return -1;
        
     }
 };
