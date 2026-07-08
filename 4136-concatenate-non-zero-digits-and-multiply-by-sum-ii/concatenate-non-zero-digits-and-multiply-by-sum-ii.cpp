37e858c7362e1729fa2e9609797b57b9d4993ba2
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-07-08 15:33:37 +0530

    Time: 39 ms (66.32%) | Memory: 155.4 MB (52.33%) - LeetSync

---
+++ 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    typedef long long ll;
+    int MOD = 1e9 + 7;
+
+    vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
+        int n = s.length();
+
+        vector<ll> digitSum(n,0);
+        vector<ll> numberUpto(n,0);
+        vector<int> nonZerodigit(n,0);
+        vector<ll> power(n+1 ,0);
+        
+        digitSum[0] = s[0] -'0';
+        for(int i = 1 ; i < n ;i++){
+            int digit = s[i] -'0';
+            if(digit != 0) digitSum[i] = (digitSum[i-1] + digit) % MOD;
+            else digitSum[i] = digitSum[i-1]; 
+        }
+
+        numberUpto[0] = s[0] -'0';
+        for(int i = 1 ; i  < n ;i++){
+            int digit = s[i] - '0';
+            if(digit != 0) numberUpto[i] = ((numberUpto[i-1] * 10) + digit) % MOD;
+            else numberUpto[i] = numberUpto[i-1];
+        }
+
+        power[0] = 1;
+        for(int i = 1 ; i < n ; i++ ){
+            power[i] = (power[i-1] * 10) % MOD;
+        }
+
+        nonZerodigit[0] = (s[0] != '0') ? 1:0;
+        for(int i = 1 ; i < n ; i++ ){
+            int digit  = s[i] - '0';
+            if(digit != 0) nonZerodigit[i] = nonZerodigit[i-1] + 1;
+            else  nonZerodigit[i] = nonZerodigit[i-1];
+        }
+
+        int q = queries.size();
+        vector<int> result(q);
+        for(int i = 0 ; i < q ; i++){
+            int l = queries[i][0];
+            int r = queries[i][1];
+
+           ll sum = digitSum[r] - ((l == 0) ? 0 : digitSum[l-1]);
+
+           int k = nonZerodigit[r] - ((l == 0) ? 0 : nonZerodigit[l-1]);
+
+           int numBefore  = (l == 0) ? 0 : numberUpto[l-1];
+
+           ll x = (numberUpto[r] - (numBefore * power[k] % MOD) + MOD ) % MOD;          
+
+           result[i] = (int)((x * sum) % MOD);
+        }
+
+        return result;
+
+
+        
+
+    }
+};
