f3ac00b13bafc2e202db5168310edf16ad7319dd
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-07-21 01:54:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (94.72%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -16,9 +16,11 @@ public:
             char ch = s[i];
 
             int idx = ch - 'a';
-
+            
+            //If any char is seen skip it
             if(taken[idx] == true) continue;
 
+            //For every character see the previous character it is lesser than it and if not then pop only when the previous char is present in future otherwise not and if present it and mark it false and pop it repeat until the previous element is smaller than current element
             while(result.length() > 0 && result.back() > ch && freq[result.back()- 'a'] > i  ){
                 taken[result.back() - 'a'] = false;
                 result.pop_back();
