907c9373b3675947bb62fc30ae306ab3006825c0
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-07-23 16:32:16 +0530

    Time: 19 ms (80.84%) | Memory: 17.4 MB (55.62%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -26,7 +26,7 @@ public:
         for(int i = 0 ; i < n ; i++){
             if(i > 0 && nums[i] == nums[i-1]) continue;
             for(int j = i + 1 ; j < n - 1 ; j++){
-                if(j > i+1 && nums[j] == nums[j-1]) continue;
+                if(j != i+1 && nums[j] == nums[j-1]) continue;
                 else{
                     int k = j + 1;
                     int l = n - 1;
