9e17be299d46eb5a61b86e34f8b1c16871430aab
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-08-01 03:16:32 +0530

    Time: 3 ms (88.86%) | Memory: 23.9 MB (30.96%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> merged_Intervals;
+
+        for (int i = 0; i < n; i++) {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+
+            if (!merged_Intervals.empty() &&
+                end <= merged_Intervals.back()[1]) {
+                continue;
+            }
+
+            for (int j = i + 1; j < n; j++) {
+                if(intervals[j][0] <= end){
+                    end = max(end, intervals[j][1]);
+                }
+                else{
+                    break;
+                }
+            }
+            merged_Intervals.push_back({start , end});
+        }
+        return merged_Intervals;
+    }
+};
