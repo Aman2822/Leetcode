09efeffc0c9af05140a37951771c7c5baf9351db
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-08-01 01:01:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (54.24%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        int l = word.length();
-        int cost = 0;
-        int count = 0 , opn_cost = 1;
+        // int l = word.length();
+        // int cost = 0;
+        // int count = 0 , opn_cost = 1;
 
-        for(int i = 0 ; i < l ; i++){
-            count++;
-            if(count > 8){
-                opn_cost++;
-                count = 1;
-            }
-            cost += opn_cost;
+        // for(int i = 0 ; i < l ; i++){
+        //     count++;
+        //     if(count > 8){
+        //         opn_cost++;
+        //         count = 1;
+        //     }
+        //     cost += opn_cost;
+        // }
+
+        // return cost;
+
+        vector<int> push(26,0);
+        int cost = 0;
+
+        for(char &ch: word){
+            push[ch -'a']++;
+        }
+        
+        sort(push.begin() ,push.end() , greater<int>());
+
+        for(int i = 0 ; i < 26 ; i++){
+            int freq = push[i];
+            int press = (i / 8) + 1;
+            cost += (freq * press);
         }
 
-        
         return cost;
+
 
 
     }
