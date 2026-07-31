6d0d7670e14d8a65d485a7cbb0fa400d4204d028
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-08-01 01:01:58 +0530

    Time: 4 ms (86.59%) | Memory: 25.4 MB (84.45%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        int l = word.length();
         vector<int> push(26,0);
         int cost = 0;
 
@@ -9,7 +8,7 @@ public:
             push[ch -'a']++;
         }
         
-        sort(push.begin() ,push.end() , greater<int>());
+        sort(push.begin() ,push.end() , greater<int>());  // For descending sort of array
 
         for(int i = 0 ; i < 26 ; i++){
             int freq = push[i];
