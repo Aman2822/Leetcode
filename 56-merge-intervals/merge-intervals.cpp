2292b0591b8fd88f57135cdd44ead5f64d4e9a5d
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-08-01 03:30:15 +0530

    Time: 3 ms (88.86%) | Memory: 23.8 MB (82.61%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -4,25 +4,37 @@ public:
         int n = intervals.size();
         sort(intervals.begin(), intervals.end());
         vector<vector<int>> merged_Intervals;
+ 
+        // Brute force
 
-        for (int i = 0; i < n; i++) {
-            int start = intervals[i][0];
-            int end = intervals[i][1];
+        // for (int i = 0; i < n; i++) {
+        //     int start = intervals[i][0];
+        //     int end = intervals[i][1];
 
-            if (!merged_Intervals.empty() &&
-                end <= merged_Intervals.back()[1]) {
-                continue;
+        //     if (!merged_Intervals.empty() && end <= merged_Intervals.back()[1]) {
+        //         continue;
+        //     }
+
+        //     for (int j = i + 1; j < n; j++) {
+        //         if(intervals[j][0] <= end){
+        //             end = max(end, intervals[j][1]);
+        //         }
+        //         else{
+        //             break;
+        //         }
+        //     }
+        //     merged_Intervals.push_back({start , end});
+        // }
+        // return merged_Intervals;
+
+        //Optimize approach 
+        for(int i = 0 ; i < n ; i++){
+            if(merged_Intervals.empty() || intervals[i][0] > merged_Intervals.back()[1]){
+                merged_Intervals.push_back(intervals[i]);
             }
-
-            for (int j = i + 1; j < n; j++) {
-                if(intervals[j][0] <= end){
-                    end = max(end, intervals[j][1]);
-                }
-                else{
-                    break;
-                }
+            else{
+                merged_Intervals.back()[1] = max(merged_Intervals.back()[1] , intervals[i][1]);
             }
-            merged_Intervals.push_back({start , end});
         }
         return merged_Intervals;
     }
