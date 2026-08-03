643abbcb9f1f09399b9de76cf9b18e90fdfcaee1
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-08-03 20:06:40 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (68.89%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n = piles.size();
+        int a_cnt = 0 , b_cnt = 0;
+        int i = 0 , j = n-1;
+
+        while( i < j){
+            if(piles[i] > piles[j]){
+                a_cnt += piles[i];
+                i++;
+            }
+            else{
+                a_cnt += piles[j];
+                j--;
+            }
+            if(piles[i] > piles[j]){
+                b_cnt += piles[j];
+                i++;
+            }
+            else{
+                b_cnt += piles[i];
+                j--;
+            }
+        }
+
+        if(a_cnt > b_cnt ) return true;
+        else return false;
+
+
     }
 };
