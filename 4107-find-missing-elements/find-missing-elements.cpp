852cfd7349bbf76354a772c6e025a3b37fefa264
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-08-04 13:00:28 +0530

    Time: 7 ms (18.86%) | Memory: 33.7 MB (17.56%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -3,18 +3,23 @@ public:
     vector<int> findMissingElements(vector<int>& nums) {
         int n = nums.size();
         vector<int> miss_elements;
-        vector<int> arr_Ele;
         unordered_set<int> temp(nums.begin() , nums.end());
         
-        int max_ele = *max_element(nums.begin() , nums.end());
-        int min_ele = *min_element(nums.begin() , nums.end());
+        // int max_ele = *max_element(nums.begin() , nums.end());
+        // int min_ele = *min_element(nums.begin() , nums.end());
+        int max_ele = INT_MIN, min_ele = INT_MAX;
+
+        for(int i = 0 ; i < n ; i++){
+           max_ele = max(max_ele, nums[i]);
+        }
+
+        for(int i = 0 ; i < n ; i++){
+           min_ele = min(min_ele, nums[i]);
+        }
 
         for(int i = min_ele + 1 ; i < max_ele ; i++){
-            if(temp.find(i) != temp.end()){
-                arr_Ele.push_back(i);
-            }
-            else{
-                miss_elements.push_back(i);
+            if(temp.find(i) == temp.end()){
+               miss_elements.push_back(i);
             }
         }
 
