762e58226fbf45b4299cc7a897c3c1bafcc8b9f3
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-08-13 16:36:52 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (46.91%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -3,10 +3,51 @@ public:
     bool search(vector<int>& nums, int target) {
         int n = nums.size();
 
-        for(int i = 0 ; i < n ; i++){
-            if(nums[i] == target) return true;
-        }
+        //Linear Search
 
+        // for(int i = 0 ; i < n ; i++){
+        //     if(nums[i] == target) return true;
+        // }
+
+        // return false;
+
+        //Binary Search
+        int low = 0 , high = n - 1;
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+
+            if(nums[mid] == target){  // If found return true
+               return true;
+            }
+
+            //edge case - [1 0 1 1 1] shrink down until this case appars left = mid = right
+            if(nums[low] == nums[mid] && nums[mid] == nums[high]){
+                low++;
+                high--;
+                continue;
+            }
+            // if it is sorted on left side then surely low will be smaller than mid
+            else if(nums[low] <= nums[mid]){  
+                if(nums[low] <= target && target <= nums[mid]){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+            //right sorted part
+            else{
+                if(nums[mid] <= target && target <= nums[high]){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid - 1;
+                }
+
+            }
+        }
         return false;
+       
     }
 };
