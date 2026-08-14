ce4467cc486454613f2f1f42d546ebef740424b9
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-08-14 09:35:31 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (12.02%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -6,7 +6,24 @@ public:
         //return nums[0];
 
         // Min element - O(n)
-        return *min_element(nums.begin() , nums.end());
+        //return *min_element(nums.begin() , nums.end());
+
+        // optimized approach
+        int n = nums.size();
+        int low  = 0 , high = n - 1;
+
+        while(low < high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] > nums[high] ){
+               low = mid + 1;
+            }
+            else{
+                high = mid;
+            }
+
+        }
+        return nums[low];
+
 
 
         
