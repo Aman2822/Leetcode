4ec281e88fe43d37e272a2aefea74536075f5632
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-08-19 13:57:33 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (93.65%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -13,37 +13,61 @@ public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         
         //Approach 1 - Brute force
-        ListNode* temp = head;
+        // ListNode* temp = head;
 
-        //Length of LL
-        int count  = 0;
-        while(temp != NULL){
-            count++;
-            temp = temp->next;
-        }
+        // //Length of LL
+        // int count  = 0;
+        // while(temp != NULL){
+        //     count++;
+        //     temp = temp->next;
+        // }
         
-        int resultant_Node = count - n;
-        //Delete the head count== n it means
-        if(count == n){
-            ListNode* newhead = head->next;
-            delete(head);
-            return newhead;
+        // int resultant_Node = count - n;
+        // //Delete the head count== n it means
+        // if(count == n){
+        //     ListNode* newhead = head->next;
+        //     delete(head);
+        //     return newhead;
+        // }
+
+        // temp = head;
+        // // To reach to the previous node that should be deleted
+        // while(temp != NULL){
+        //     resultant_Node--;
+
+        //     if(resultant_Node == 0){
+        //         break;
+        //     }
+        //     temp = temp->next;
+        // }
+        // //Require delete Node
+        // ListNode* deleteNode = temp->next;
+        // temp->next = temp->next->next;  // Change of address
+        // delete(deleteNode);
+
+        // return head;
+
+        //Approach 2 - Optimal- Slow and fast pointers
+        ListNode* fast = head;
+        
+        //Move forward n steps
+        for(int i = 0 ; i < n ; i++){
+            fast = fast->next;
         }
 
-        temp = head;
-        // To reach to the previous node that should be deleted
-        while(temp != NULL){
-            resultant_Node--;
-
-            if(resultant_Node == 0){
-                break;
-            }
-            temp = temp->next;
+        if(fast == NULL){
+            return head->next;
         }
-        //Require delete Node
-        ListNode* deleteNode = temp->next;
-        temp->next = temp->next->next;  // Change of address
-        delete(deleteNode);
+
+        ListNode* slow = head;
+        while(fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next;
+        }
+
+        ListNode* delNode = slow->next;
+        slow->next = slow->next->next;
+        delete(delNode);
 
         return head;
     }
