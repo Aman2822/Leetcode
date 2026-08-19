e27ba99e13eadc780e2fe16f379ce16299c713ee
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-08-20 00:54:56 +0530

    Time: 12 ms (93.41%) | Memory: 60.2 MB (73.48%) - LeetSync

--- 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
@@ -2,27 +2,64 @@ class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
 
+        // //Approach - 1
+        // unordered_map<int , unordered_set<int>> mp; // For storing of reserved seats row and seat
+
+        // for(auto& reserved : reservedSeats){
+        //     int row = reserved[0];
+        //     int seat = reserved[1];
+
+        //     mp[row].insert(seat);
+        // }
+
+        // int result = (n - mp.size()) * 2; // this will give the no of unreservedseats and max 2 groups can be there in one row
+
+        // for(auto& [row , bookedSeats] : mp){ //Key and value
+
+        //     auto isAvailable = [&](int seat){
+        //         return bookedSeats.find(seat) == bookedSeats.end(); // return true if empty seat
+        //     };
+
+        //     bool groupA = isAvailable(2) && isAvailable(3) && isAvailable(4) && isAvailable(5);
+        //     bool groupB = isAvailable(4) && isAvailable(5) && isAvailable(6) && isAvailable(7);
+        //     bool groupC = isAvailable(6) && isAvailable(7) && isAvailable(8) && isAvailable(9);
+
+        //     if(groupA && groupC){
+        //         result += 2;
+        //     }
+        //     else if(groupA || groupB || groupC){
+        //         result += 1;
+        //     }
+        // }
+
+        // return result;
+
+        
+        //Approach 2 - Reduced space complexity - Bit Manipulation
+
         //Approach - 1
-        unordered_map<int , unordered_set<int>> mp; // For storing of reserved seats row and seat
+        unordered_map<int , int > mp; // For storing of reserved seats row and seat
 
         for(auto& reserved : reservedSeats){
             int row = reserved[0];
             int seat = reserved[1];
 
-            mp[row].insert(seat);
+            mp[row] = (mp[row] | 1 << seat);
         }
 
         int result = (n - mp.size()) * 2; // this will give the no of unreservedseats and max 2 groups can be there in one row
 
-        for(auto& [row , bookedSeats] : mp){ //Key and value
+        int maskA = 1 << 2 | 1 << 3 | 1 << 4 | 1 << 5;
+        int maskB = 1 << 4 | 1 << 5 | 1 << 6 | 1 << 7;
+        int maskC = 1 << 6 | 1 << 7 | 1 << 8 | 1 << 9;
 
-            auto isAvailable = [&](int seat){
-                return bookedSeats.find(seat) == bookedSeats.end(); // return true if empty seat
-            };
 
-            bool groupA = isAvailable(2) && isAvailable(3) && isAvailable(4) && isAvailable(5);
-            bool groupB = isAvailable(4) && isAvailable(5) && isAvailable(6) && isAvailable(7);
-            bool groupC = isAvailable(6) && isAvailable(7) && isAvailable(8) && isAvailable(9);
+        for(auto& [row , bookedSeatsMask] : mp){ //Key and value
+
+
+            bool groupA = (bookedSeatsMask & maskA) == 0;
+            bool groupB = (bookedSeatsMask & maskB) == 0;
+            bool groupC = (bookedSeatsMask & maskC) == 0; 
 
             if(groupA && groupC){
                 result += 2;
@@ -33,6 +70,8 @@ public:
         }
 
         return result;
+
+
 
         
     }
