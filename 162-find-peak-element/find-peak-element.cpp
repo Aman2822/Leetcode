59412b9a45c32e81804dce73e439854863a8cb82
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-08-21 09:15:29 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (8.89%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -16,19 +16,46 @@ public:
         // }
         // return max_ind;
 
-        //Approach - 2
+        //Approach - 2 - O(N)
 
-        for(int i = 0 ; i < n ; i++){
-           //Check for left index
-           bool left = (i == 0 ) || (nums[i-1] < nums[i]);
-           //Check for right index
-           bool right = (i == n - 1) || (nums[i] > nums[i+1]);
+        // for(int i = 0 ; i < n ; i++){
+        //    //Check for left index
+        //    bool left = (i == 0 ) || (nums[i-1] < nums[i]);
+        //    //Check for right index
+        //    bool right = (i == n - 1) || (nums[i] > nums[i+1]);
 
-           //If both are valid then peak element exists
-           if(left && right) return i;
+        //    //If both are valid then peak element exists
+        //    if(left && right) return i;
+        // }
+
+        // return -1;
+
+        //Approach 3 - Binary Search
+
+        if(n == 1) return 0;
+
+        //Check for first element and last element
+        if(nums[0] > nums[1]) return 0;
+        if(nums[n-1] > nums[n-2]) return n-1;
+
+        int low = 1 , high = n-2;
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+
+            if(nums[mid-1] < nums[mid] && nums[mid] > nums[mid+1]){  // Peak condition
+                return mid;
+            }
+            else if(nums[mid] > nums[mid-1]){ // means it is on a linear curve
+                low = mid + 1;
+            }
+            else{                               
+                high = mid - 1;
+            }
         }
 
         return -1;
+
 
 
 
