7cb27ced62c4c29859b224bdbb9e2dc4b5155783
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-08-21 08:35:24 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (75.67%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -4,17 +4,34 @@ public:
         int n = nums.size();
 
         //Brute Force - Return the max element index
-        int max_ele = nums[0];
-        int max_ind = 0;
+        
+        // int max_ele = nums[0];
+        // int max_ind = 0;
 
+        // for(int i = 0 ; i < n ; i++){
+        //     if(nums[i] > max_ele){
+        //         max_ele = nums[i];
+        //         max_ind = i;
+        //     }
+        // }
+        // return max_ind;
+
+        //Approach - 2
 
         for(int i = 0 ; i < n ; i++){
-            if(nums[i] > max_ele){
-                max_ele = nums[i];
-                max_ind = i;
-            }
+           //Check for left index
+           bool left = (i == 0 ) || (nums[i-1] < nums[i]);
+           //Check for right index
+           bool right = (i == n - 1) || (nums[i] > nums[i+1]);
+
+           //If both are valid then peak element exists
+           if(left && right) return i;
         }
-        return max_ind;
+
+        return -1;
+
+
+
 
     }
 };
