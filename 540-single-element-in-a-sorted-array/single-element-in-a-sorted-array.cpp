34aae70feab7e4bca2de7c6acdbf1588c8722ebe
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-08-26 01:05:12 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (37.00%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -17,16 +17,45 @@ public:
         // return 0;
 
         //Approach 2 - Like linear search - O(N)
-        if(n == 1) return nums[0];
-        if(nums[0] != nums[1]) return nums[0];
-        if(nums[n-1] != nums[n-2]) return nums[n-1];
+        // if(n == 1) return nums[0];
+        // if(nums[0] != nums[1]) return nums[0];
+        // if(nums[n-1] != nums[n-2]) return nums[n-1];
 
-        for(int i = 1 ; i < n-1 ; i++){
-            if(nums[i] != nums[i+1] && nums[i] != nums[i-1]){ // mid ele is not same as both left & right
-                return nums[i];
-            }
-        }
+        // for(int i = 1 ; i < n-1 ; i++){
+        //     if(nums[i] != nums[i+1] && nums[i] != nums[i-1]){ // mid ele is not same as both left & right
+        //         return nums[i];
+        //     }
+        // }
         
-        return -1;
+        // return -1;
+
+       //Approach - 3 : Binary Search 
+
+       if(n == 1) return nums[0];                       
+       if(nums[0] != nums[1]) return nums[0];
+       if(nums[n-1] != nums[n-2]) return nums[n-1];
+
+       int low = 1 , high = n - 2;    //Because i have check last and first digit check earlier
+
+       while(low <= high){
+        int mid = low + (high - low)/2;
+        if(nums[mid] != nums[mid+1] && nums[mid] != nums[mid-1]){
+            return nums[mid];
+        }
+        // Suppose [1,1,2,2,3,3,4,5,5,6,6] 
+        // (even , odd) ,(even,odd) ,(even,odd) are the groups in the left part of single element 
+        //(odd , even) like this are the groups in the right part of single element
+
+        // we are on left part
+        else if((mid % 2 == 1 && nums[mid] == nums[mid-1]) || (mid % 2 == 0 && nums[mid] == nums[mid+1])){
+            low = mid + 1;
+        }
+        //we are on right part
+        else{
+            high = mid - 1;
+        }
+       }
+
+       return -1;
     }
 };
