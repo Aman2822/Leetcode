abe8974d4f795a8d1349d9b28cd06d5cac811583
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-08-26 00:24:39 +0530

    Time: 0 ms (100.00%) | Memory: 26.4 MB (7.42%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -2,15 +2,31 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int,int> mp;
 
-        for(int i = 0 ; i < n ; i++){
-            mp[nums[i]]++;
-        }
+        //Brute force- Hashmap
 
-        for(auto it : mp){
-            if(it.second == 1) return it.first;
+        // unordered_map<int,int> mp;
+
+        // for(int i = 0 ; i < n ; i++){
+        //     mp[nums[i]]++;
+        // }
+
+        // for(auto it : mp){
+        //     if(it.second == 1) return it.first;
+        // }
+        // return 0;
+
+        //Approach 2 - Like linear search - O(N)
+        if(n == 1) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[n-1] != nums[n-2]) return nums[n-1];
+
+        for(int i = 1 ; i < n-1 ; i++){
+            if(nums[i] != nums[i+1] && nums[i] != nums[i-1]){ // mid ele is not same as both left & right
+                return nums[i];
+            }
         }
-        return 0;
+        
+        return -1;
     }
 };
