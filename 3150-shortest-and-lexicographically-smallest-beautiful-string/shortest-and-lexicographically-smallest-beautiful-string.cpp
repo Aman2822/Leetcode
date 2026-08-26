32e97d4488f362626d0d65d48fabdd386c6bb33b
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-08-26 18:09:23 +0530

    Time: 3 ms (26.70%) | Memory: 8.7 MB (82.87%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -2,53 +2,59 @@ class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
         int l = s.length();
-        // unordered_map<int ,int> mp;  //Sliding Window and Hashmap
-        // vector<string> beautiful;
+        //Sliding Window and Hashmap
+        string beautiful = "";
 
-        // int i = 0 , j = 0;
+        int i = 0 , j = 0;
+        int ones = 0;
 
-        // while(j < l){
-        //     mp[s[j]]++;
+        while(j < l){
+            if(s[j] == '1'){
+                ones++;
+            }
 
-        //     while( i < j && mp.count(1) > k){
-        //         mp[s[i]]--;
-        //         i++;
-        //     }
-
-        //     if(mp.count(1) == k){
-        //       string res = s.substr(i , j-i+1);
-
-        //     }
-        //     j++;
-        // }
-
-        // for(int i = 0 ; i < beautiful.size() ; i++){
-        //     cout << beautiful[i] << " ";
-        // }
-
-        // return s;
-
-        for (int len = k; len <= l; len++) { // k size ka string toh chahiye
-            string result = "";
-
-            for (int j = 0; j <= l - len; j++) {
-                string temp = s.substr(j, len); // substring
-
-                int ones = 0;
-                for (char& ch : temp) {    //for count 1's
-                    ones += (ch == '1') ? 1 : 0;
-                }
-
-                if (ones == k) {   //If ones count=k then  result is empty we can add or smaller than result
-                    if (result.empty() || temp < result) {
-                        result = temp;
-                    }
+            // Shrinking logic when ones is greater than k then shrink down upto we see 0
+            while(ones > k || s[i] == '0'){ 
+                if(s[i] == '1') ones--;
+                i++;
+            }
+    
+            if(ones == k){
+                string temp = s.substr(i , j-i+1);
+                if(beautiful.empty() || beautiful.length() > j-i+1 || 
+                (temp.length() == beautiful.length() &&  temp < beautiful)){
+                    beautiful = temp;
                 }
             }
-            if(!result.empty()) return result;     //whenever we found the result we should it because it is lexicographically smaller than future one
-
+            j++;
         }
 
-        return "";
+
+        return beautiful;
+
+        //Brute Force - TC = O(N*N*N)
+
+        // for (int len = k; len <= l; len++) { // k size ka string toh chahiye
+        //     string result = "";
+
+        //     for (int j = 0; j <= l - len; j++) {
+        //         string temp = s.substr(j, len); // substring
+
+        //         int ones = 0;
+        //         for (char& ch : temp) {    //for count 1's
+        //             ones += (ch == '1') ? 1 : 0;
+        //         }
+
+        //         if (ones == k) {   //If ones count=k then  result is empty we can add or smaller than result
+        //             if (result.empty() || temp < result) {
+        //                 result = temp;
+        //             }
+        //         }
+        //     }
+        //     if(!result.empty()) return result;     //whenever we found the result we should it because it is lexicographically smaller than future one
+
+        // }
+
+        // return "";
     }
 };
