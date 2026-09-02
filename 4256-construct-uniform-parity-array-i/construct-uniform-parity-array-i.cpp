f5b53d7b6b3034bd746f6ac11bf0ecc245da7526
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-09-02 13:02:18 +0530

    Time: 0 ms (100.00%) | Memory: 30.3 MB (36.43%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int n = nums1.size();
-        vector<int> nums2;
+        // int n = nums1.size();
+        // vector<int> nums2;
 
-        if (n == 1) {
-            return true;
-        }
+        // if (n == 1) {
+        //     return true;
+        // }
 
-        // Check only 1st and 2  elements;
-        if (nums1[0] % 2 != nums1[1] % 2) {
-            for (int i = 0; i < n-1 ; i++) {
-                if (nums1[i] % 2 == 1) {
-                    nums2.push_back(nums1[i]);
-                } else {
-                    nums2.push_back(nums1[i] - nums1[i+1]);
-                }
-            }
-            if (nums1[n-1] % 2 == 1) {
-                nums2.push_back(nums1[n-1]);
-            } else {
-                nums2.push_back(nums1[n-2] - nums1[n-1]);
-            }
-        }
+        // // Check only 1st and 2  elements;
+        // if (nums1[0] % 2 != nums1[1] % 2) {
+        //     for (int i = 0; i < n-1 ; i++) {
+        //         if (nums1[i] % 2 == 1) {
+        //             nums2.push_back(nums1[i]);
+        //         } else {
+        //             nums2.push_back(nums1[i] - nums1[i+1]);
+        //         }
+        //     }
+        //     if (nums1[n-1] % 2 == 1) {
+        //         nums2.push_back(nums1[n-1]);
+        //     } else {
+        //         nums2.push_back(nums1[n-2] - nums1[n-1]);
+        //     }
+        // }
 
-        if (nums1[0] % 2 == nums1[1] % 2) {
-            for (int i = 0; i < n; i++) {
-                nums2.push_back(nums1[i]);
-            }
-        }
+        // if (nums1[0] % 2 == nums1[1] % 2) {
+        //     for (int i = 0; i < n; i++) {
+        //         nums2.push_back(nums1[i]);
+        //     }
+        // }
 
         // bool even = false;
         // bool odd = false;
