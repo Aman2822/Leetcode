80f479a21fae4ec65170c8901f01742810a014b4
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-09-05 17:56:02 +0530

    Time: 7 ms (69.43%) | Memory: 23 MB (58.68%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    typedef long long ll;
+    ll helper(vector<int>& piles , int hourly){
+        ll sum = 0;
+
+        for(int i = 0 ; i < piles.size() ; i++){
+            sum += (piles[i] + hourly - 1) / hourly;
+        }
+
+        // cout << sum << endl;
+
+        return sum;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int maxi = *max_element(piles.begin() , piles.end());
+
+        //Binary Search 
+        int low = 1 , high = maxi;
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+
+            ll requireTime = helper(piles, mid);
+
+            if(requireTime <= h){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return low;
+
+    }
+};
