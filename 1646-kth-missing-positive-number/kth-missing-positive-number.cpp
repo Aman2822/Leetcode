a1d7e1450cfe01f17c3f4982f9a27c178bb42590
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-09-10 18:41:04 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (60.88%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -2,22 +2,40 @@ class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
         int n = arr.size();
-        unordered_map<int , int> mp;
+        // unordered_map<int , int> mp;
 
-        for(int i = 0 ; i < n ; i++ ){
-            mp[arr[i]]++;
-        }
-        int i = 1;
-        int ans = -1;
-        while(k > 0){
-            if(mp.find(i) == mp.end()){
-                ans = i;
-                k--;
+        // for(int i = 0 ; i < n ; i++ ){
+        //     mp[arr[i]]++;
+        // }
+        // int i = 1;
+        // int ans = -1;
+        // while(k > 0){
+        //     if(mp.find(i) == mp.end()){
+        //         ans = i;
+        //         k--;
+        //     }
+        //     i++;
+        // }
+
+        // return ans;
+
+        //Binary Search
+        int low = 0 , high = n - 1;
+
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+
+            int missing = arr[mid] - (mid+1);    //Missing integers upto mid index
+
+            if(missing < k){
+                low = mid + 1;
             }
-            i++;
+            else{
+                high = mid - 1;
+            }
         }
 
-        return ans;
+        return high + k + 1;               //arr[high] + more    more = (k- missing)
 
     }
 };
