f145f66af2c94539578bfa5e9ca6bdd73037c32a
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-09-12 00:17:16 +0530

    Time: 4 ms (68.91%) | Memory: 14 MB (49.12%) - LeetSync

--- 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
@@ -4,23 +4,57 @@ public:
         int n = digits.size();
         
         //Approach 1 - all possibilities
+        // set<int> ans;
+        // for(int i = 0 ; i < n ; i++){
+        //     for(int j = 0 ; j < n ; j++){
+        //         for(int k = 0 ; k < n ; k++){
+        //             if(i == j || j == k || i == k) continue;
+
+        //             int num = (digits[i] * 100) + (digits[j] * 10) + digits[k];
+
+        //             if(num > 99 &&  num % 2 == 0){
+        //                 ans.insert(num);
+        //             }
+        //         }
+        //     }
+        // }
+
+        // vector<int> result(ans.begin() , ans.end());
+
+        // return result;
+
+        //Approach 2 - Using map
         set<int> ans;
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < n ; j++){
-                for(int k = 0 ; k < n ; k++){
-                    if(i == j || j == k || i == k) continue;
+        vector<int> mp(10 , 0);  //Created a map with size 10 intilaized to 0
+        
+        for(int &nums : digits){
+            mp[nums]++;
+        }
 
-                    int num = (digits[i] * 100) + (digits[j] * 10) + digits[k];
+        for(int i = 1 ; i <= 9 ; i++){
+            if(mp[i] == 0) continue;
+            mp[i]--;
+            for(int j = 0 ; j <= 9 ; j++){
+                if(mp[j] == 0) continue;
+                mp[j]--;
+                for(int k = 0 ; k <=8 ; k += 2){
+                    if(mp[k] == 0) continue;
+                    mp[k]--;
 
-                    if(num > 99 &&  num % 2 == 0){
+                    int num = (i * 100) + (j * 10) + k;
+                    
+                    if(num > 99 & num % 2 == 0){
                         ans.insert(num);
                     }
+
+                    mp[k]++;
                 }
+                mp[j]++;
             }
+            mp[i]++;
         }
 
         vector<int> result(ans.begin() , ans.end());
-
         return result;
     }
 };
