21817e3704de5899f0c5706adfc1967e1d648c66
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-09-20 18:20:34 +0530

    Time: 7 ms (5.47%) | Memory: 12.4 MB (5.11%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int n = s.length();
+
+        //Make a pair of char and there value and store them in 2d array
+        vector<pair<char,int>> values;
+        int i = 0;
+
+        for(int k = 26; k >= 1 ; k-- ){
+            char ch = 'a' + i;
+            values.push_back({ch , k});
+            i++;
+        }
+
+        unordered_map<char , int> mp(values.begin() , values.end());
+        long long prod = 0;
+        for(int i = 0 ; i < n ; i++){
+          int val = mp[s[i]];
+          prod += (val * (i+1));
+        }
+
+        return prod;
+    }
+};
