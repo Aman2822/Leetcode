6f326ffbf97c5e4acf47f478a66a5c38a4d8e08e
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-09-20 18:30:34 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (47.88%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -3,23 +3,35 @@ public:
     int reverseDegree(string s) {
         int n = s.length();
 
-        //Make a pair of char and there value and store them in 2d array
-        vector<pair<char,int>> values;
-        int i = 0;
+        // //Make a pair of char and there value and store them in 2d array
+        // vector<pair<char,int>> values;
+        // int i = 0;
 
-        for(int k = 26; k >= 1 ; k-- ){
-            char ch = 'a' + i;
-            values.push_back({ch , k});
-            i++;
-        }
+        // for(int k = 26; k >= 1 ; k-- ){
+        //     char ch = 'a' + i;
+        //     values.push_back({ch , k});
+        //     i++;
+        // }
 
-        unordered_map<char , int> mp(values.begin() , values.end());
+        // unordered_map<char , int> mp(values.begin() , values.end());
         long long prod = 0;
+        // for(int i = 0 ; i < n ; i++){
+        //   int val = mp[s[i]];
+        //   prod += (val * (i+1));
+        // }
+
+        // return prod;
+
+
+
+
+        //One more approach
+        
         for(int i = 0 ; i < n ; i++){
-          int val = mp[s[i]];
-          prod += (val * (i+1));
+            prod += (i+1) * (26 - (s[i] - 'a'));
         }
 
         return prod;
+
     }
 };
