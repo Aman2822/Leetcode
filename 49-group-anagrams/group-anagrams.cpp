27d8b701314bdf625b299973f13c72f40522ea40
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-09-21 23:43:53 +0530

    Time: 22 ms (32.07%) | Memory: 27.2 MB (23.67%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,20 +1,53 @@
 class Solution {
 public:
-   /*
-   humlog sabka ascii vlaue nikal sakte hai and pair kar sakte hai and fir jiska values equal usko ek array mein dal ke push kar sakte hai 
-   */
+    string anagram(string curr){
+        int arr[26] = {0};
+
+        for(char &ch : curr){
+            arr[ch - 'a']++;
+        }
+
+        string formed = "";
+
+        for(int i = 0 ; i < 26 ; i++){
+            int freq = arr[i];
+
+            if(freq > 0){
+                formed += string(freq , i + 'a');
+            }
+        }
+
+        return formed;
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         int n = strs.size();
         vector<vector<string>> ans;
 
-        //Approach I - sort internally
+        // //Approach I - sort internally
         
+        // unordered_map<string , vector<string>> mp;
+
+        // for(const string &s : strs ){
+        //     string key = s;
+        //     sort(key.begin() , key.end());
+        //     mp[key].push_back(s);
+        // }
+
+        // for(auto it : mp){
+        //     ans.push_back(it.second);
+        // }
+
+        // return ans;
+
+        //Approach 2 - without sorting
         unordered_map<string , vector<string>> mp;
 
-        for(const string &s : strs ){
-            string key = s;
-            sort(key.begin() , key.end());
-            mp[key].push_back(s);
+        for(string &s : strs){
+           string curr = s;
+
+           string formed = anagram(curr);
+
+           mp[formed].push_back(curr);
         }
 
         for(auto it : mp){
@@ -22,6 +55,8 @@ public:
         }
 
         return ans;
+
+
 
     }
 };
