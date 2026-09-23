a30aa30d8438093a9253979ba036883250970652
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-09-23 12:45:05 +0530

    Time: 3 ms (31.98%) | Memory: 13.4 MB (66.10%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -3,16 +3,40 @@ public:
     int singleNumber(vector<int>& nums) {
 
         // Approach 1 - Hashing - TC = O(N) AND SC = O(N)
-        unordered_map<int , int> mp;
+        // unordered_map<int , int> mp;
 
-        for(int &num : nums){
-           mp[num]++;
+        // for(int &num : nums){
+        //    mp[num]++;
+        // }
+
+        // for(auto it : mp){
+        //     if(it.second == 1) return it.first;
+        // }
+
+        // return -1;
+
+        //Approach 2 - Bit Manipulation
+        int n = nums.size();
+        int ans  = 0; 
+
+        //Count every place one's if its divisible by 3 if yes then add to the ans 
+
+        for(int bitIdx = 0 ; bitIdx < 32 ; bitIdx++){
+           int cnt = 0;
+           for(int i = 0 ; i < n ; i++){
+            if(nums[i] & (1 << bitIdx)){
+                cnt++;
+            }
+           }
+
+           if(cnt % 3 == 1){
+            ans = ans | (1 << bitIdx);
+           }
         }
 
-        for(auto it : mp){
-            if(it.second == 1) return it.first;
-        }
+        return ans;
+        
 
-        return -1;
+
     }
 };
