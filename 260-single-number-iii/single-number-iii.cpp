be47b725df3e74dec4cb405479134689cade9142
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-09-24 15:33:42 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (57.61%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -4,9 +4,9 @@ public:
         int n = nums.size();
         if(n == 2) return nums;
 
-        //Approach 1 - Hashmap
+        //Approach 1 - Hashmap - TC = O(N logM) + M   and SC O(M) ,  m is map size
         // unordered_map<int, int> mp;
-        vector<int> ans;
+        // vector<int> ans;
 
         // for(int &num : nums){
         //     mp[num]++;
@@ -21,24 +21,46 @@ public:
         // return ans;
 
 
-        //Approach 2 - Bit Manipulation
-        sort(begin(nums) , end(nums));
+        //Approach 2 - Sorting - TC = O(N logN) + N   SC = O(1)
+        // sort(begin(nums) , end(nums));
 
-        for(int i = 0 ; i < n - 1 ; i++){
-           int val = nums[i] ^ nums[i+1];
-           if(val == 0){
-            i++;
-           }
-           else{
-            ans.push_back(nums[i]);
-           }
+        // for(int i = 0 ; i < n - 1 ; i++){
+        //    int val = nums[i] ^ nums[i+1];
+        //    if(val == 0){
+        //     i++;
+        //    }
+        //    else{
+        //     ans.push_back(nums[i]);
+        //    }
+        // }
+
+        // if(ans.size() == 1){
+        //     ans.push_back(nums[n-1]);
+        // }
+
+        // return ans;
+
+        //Approach 3 - with help of buckets and bit manipulation  tC= O(2N) and SC = O(1)
+        long xorr = 0;
+        for(int i = 0 ; i < n ; i++){
+            xorr = xorr ^ nums[i];
         }
 
-        if(ans.size() == 1){
-            ans.push_back(nums[n-1]);
+        int rightmost = (xorr & xorr - 1) ^ xorr;
+        int b1 = 0 , b2 = 0;
+
+        for(int i = 0 ; i < n ; i++){
+            //with the help of that rightmost seperate nums in two buckets
+            if(rightmost & nums[i]){
+                b1 = b1 ^ nums[i];
+            }
+            else{
+                b2 = b2 ^ nums[i];
+            }
         }
 
-        return ans;
+        return {b1 , b2};
+
 
 
         
