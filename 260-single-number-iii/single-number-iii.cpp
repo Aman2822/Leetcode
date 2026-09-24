e66758023caac29a3cc0bd02150ac151a92748f4
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-09-24 13:33:41 +0530

    Time: 4 ms (16.61%) | Memory: 14 MB (31.61%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -2,23 +2,46 @@ class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
         int n = nums.size();
+        if(n == 2) return nums;
 
         //Approach 1 - Hashmap
-        unordered_map<int, int> mp;
+        // unordered_map<int, int> mp;
         vector<int> ans;
 
-        for(int &num : nums){
-            mp[num]++;
+        // for(int &num : nums){
+        //     mp[num]++;
+        // }
+
+        // for(auto it : mp){
+        //     if(it.second == 1){
+        //         ans.push_back(it.first);
+        //     }
+        // }
+
+        // return ans;
+
+
+        //Approach 2 - Bit Manipulation
+        sort(begin(nums) , end(nums));
+
+        for(int i = 0 ; i < n - 1 ; i++){
+           int val = nums[i] ^ nums[i+1];
+           if(val == 0){
+            i++;
+           }
+           else{
+            ans.push_back(nums[i]);
+           }
         }
 
-        for(auto it : mp){
-            if(it.second == 1){
-                ans.push_back(it.first);
-            }
+        if(ans.size() == 1){
+            ans.push_back(nums[n-1]);
         }
 
         return ans;
 
+
+        
 
     }
 };
