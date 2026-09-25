a41513c1d44c86438a3f1828da8dbbc2752a4867
Aman Amarjeet Maurya <132164788+Aman2822@users.noreply.github.com>
2026-09-25 13:04:50 +0530

    Time: 3 ms (67.42%) | Memory: 13.3 MB (74.77%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -2,48 +2,84 @@ class Solution {
 public:
     vector<string> result;
 
-    bool isValid(string& str){
-        int cnt = 0;
+    // Approach 1 - All strings generated and then check valid or invalid
+    //  bool isValid(string& str){
+    //      int cnt = 0;
 
-        for(char &ch : str){
-            if(ch == '('){
-                cnt++;
-            }
-            else{
-                cnt--;
-            }
+    //     for(char &ch : str){
+    //         if(ch == '('){
+    //             cnt++;
+    //         }
+    //         else{
+    //             cnt--;
+    //         }
 
-            if(cnt < 0) return false;
-        }
+    //         if(cnt < 0) return false;
+    //     }
 
-        return cnt == 0;
-    }
+    //     return cnt == 0;
+    // }
 
-    void solve(string& s , int n){
-        if(s.length() == 2*n){
-            if(isValid(s)){
-                result.push_back(s);
-            }
+    // void solve(string& s , int n){
+    //     if(s.length() == 2*n){
+    //         if(isValid(s)){
+    //             result.push_back(s);
+    //         }
+    //         return;
+    //     }
+
+    //     //Two possilibilites
+
+    //     //open bracket
+    //     s.push_back('(');
+    //     solve(s , n);
+    //     s.pop_back();
+
+    //     //close bracket
+    //     s.push_back(')');
+    //     solve(s , n);
+    //     s.pop_back();
+
+    // }
+    // vector<string> generateParenthesis(int n) {
+    //     string curr = "";
+
+    //     solve(curr , n);
+
+    //     return result;
+    // }
+
+    // Approach 2 - only generate the strings which are valid  we will be saving time 
+    // valid check time
+    void solve(string& s, int n, int open, int close) {
+        if (s.length() == 2 * n) {
+            result.push_back(s);
             return;
         }
 
-        //Two possilibilites
+        // Two possilibilites
 
-        //open bracket
-        s.push_back('(');
-        solve(s , n);
-        s.pop_back();
+        // open bracket when open < n
 
-        //close bracket
-        s.push_back(')');
-        solve(s , n);
-        s.pop_back();
+        if (open < n) {
+            s.push_back('(');
+            solve(s, n, open + 1, close);
+            s.pop_back();
+        }
 
+        // close bracket when close < open
+        if (close < open) {
+            s.push_back(')');
+            solve(s, n, open, close + 1);
+            s.pop_back();
+        }
     }
     vector<string> generateParenthesis(int n) {
         string curr = "";
 
-        solve(curr , n);
+        // int open = 0 , close = 0;
+
+        solve(curr, n, 0, 0);
 
         return result;
     }
